a753021519e644ad1da5bbc8938e3a0582d1b083
Banul <banul2@wp.pl>
2018-08-25 11:53:53 +0200

    pierwszy commit, zainicjalizowany projekt

---
+++ src/main/java/pl/pasieka/Pasieka.java
@@ -0,0 +1,12 @@
+package pl.pasieka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pasieka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pasieka.class, args);
+	}
+}
